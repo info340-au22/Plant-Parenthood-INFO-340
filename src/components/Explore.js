508065785f4fb7af63dff3408476c7ce4c4e8b69
Saimanasvi <smcharu@uw.edu>
2022-11-22 14:54:28 -0800

    explore search changes

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export function PlantCard(props) {
     console.log(props.plants.name);
 
-<<<<<<< HEAD
     return(
         //<div>
             
@@ -27,8 +26,6 @@ export function PlantList(props) {
         <PlantCard key={plant.name} plants={plant} />
     );
 
-=======
->>>>>>> 8fa00b69e6ff9572ef624ad9fa1472d0e8144ab1
     return (
         <div className="explore-item">
             <h3>{props.plants.name}</h3>
@@ -39,31 +36,13 @@ export function PlantList(props) {
     )
 }
 
-<<<<<<< HEAD
 export function Search() {
     const [query, setQuery] = useState("");
     return (
         <div>
             <input type="text" placeHolder="Search..." onChange={e=> setQuery(e.target.value)}/>
-            <PLantList></PLantList>
+            <PlantList></PlantList>
         </div>
-=======
-export function PlantList(props) {
-    const plants = props.plants.map((plant) =>
-        <PlantCard key={plant.name} plants={plant} />
-    );
-
-    return (
-        <>
-            <div>
-                <h1 className="explore-heading">Find your perfect plant!</h1>
-                <h2 className="explore-subheading">Search for different species of houseplants in our houseplant index.</h2>
-                <div className="explore-container">
-                    {plants}
-                </div>
-            </div>
-        </>
->>>>>>> 8fa00b69e6ff9572ef624ad9fa1472d0e8144ab1
     )
 }
 
