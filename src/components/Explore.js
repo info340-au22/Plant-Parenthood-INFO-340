949544a1f44fb35633286f6de0dfc317922ad732
Saimanasvi <smcharu@uw.edu>
2022-11-22 15:19:24 -0800

    search component finish

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import { useState } from 'react';
+
 
 export function PlantCard(props) {
-    console.log(props.plants.name);
-
-    return(
-        //<div>
-            
-                <div className="explore-item">
-                    <h3>{props.plants.name}</h3>
-                    <div className="plant-img">
-                        <img src={props.plants.img} alt={props.plants.alt}></img>
-                    </div>
-                </div>
-        //</div>
-    )
-} 
-
-export function PlantList(props) {
-    // const plants = props.plants.map((plant) => 
-    //     <PlantCard key={plant.name} plants={plant} />
-    // );
-
-    const plants = props.plants.filter(plant=>plant.name.toLowerCase().includes(query)).map((plant) => 
-        <PlantCard key={plant.name} plants={plant} />
-    );
 
     return (
         <div className="explore-item">
@@ -36,12 +15,21 @@ export function PlantList(props) {
     )
 }
 
-export function Search() {
+export function PlantList(props) {
     const [query, setQuery] = useState("");
+
+    const plants = props.plants.filter(plant=>plant.name.toLowerCase().includes(query)).map((plant) => 
+        <PlantCard key={plant.name} plants={plant} />
+    );
+
     return (
         <div>
-            <input type="text" placeHolder="Search..." onChange={e=> setQuery(e.target.value)}/>
-            <PlantList></PlantList>
+            <h1 className="explore-heading">Find your perfect plant!</h1>
+            <h2 className="explore-subheading">Search for different species of houseplants in our houseplant index.</h2>
+            <input type="text" placeholder="Search..." onChange={(e)=> setQuery(e.target.value)}/>
+            <div className="explore-container">
+                {plants}
+            </div>
         </div>
     )
 }
