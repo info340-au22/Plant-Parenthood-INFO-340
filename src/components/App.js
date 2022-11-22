1418c5a583d972147be15c1a2b35152f880bbbb9
Kriti Vajjhula <avajjh@uw.edu>
2022-11-22 14:54:03 -0800

    make navbar hamburger

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Routes, Route, Link} from "react-router-dom";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import { Routes, Route, Link } from "react-router-dom";
 import { Home } from "./Home.js";
 import { Quiz } from "./Quiz.js";
 import { Calendar } from "./Calendar.js";
@@ -14,30 +16,33 @@ import { About } from "./About.js"
 export default function App(props) {
     return (
         <div>
-        <>
-            <Navbar className="color-Nav" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">Plant Parenthood</Navbar.Brand>
-                    <Nav className="me-auto">
-                        {/* <Nav.Link as={Link} to='/'>Home</Nav.Link> */}
-                        <Nav.Link as={Link} to='/Quiz.js'>Quiz</Nav.Link>
-                        <Nav.Link as={Link} to='/Calendar.js'>Calendar</Nav.Link>
-                        <Nav.Link as={Link} to='/Explore.js'>Explore</Nav.Link>
-                        <Nav.Link as={Link} to='/About.js'>About</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-        <div>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/Quiz.js" element={<Quiz/>}/>
-                <Route path="/Calendar.js" element={<Calendar/>}/>
-                <Route path="/Explore.js" element={<PlantList  plants={props.plants}/>}/>
-                <Route path="/About.js" element={<About/>}/>
-            </Routes>
+            <>
+                <Navbar className="color-Nav" variant="dark" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="/">Plant Parenthood</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            {/* <Nav.Link as={Link} to='/'>Home</Nav.Link> */}
+                            <Nav.Link as={Link} to='/Quiz.js'>Quiz</Nav.Link>
+                            <Nav.Link as={Link} to='/Calendar.js'>Calendar</Nav.Link>
+                            <Nav.Link as={Link} to='/Explore.js'>Explore</Nav.Link>
+                            <Nav.Link as={Link} to='/About.js'>About</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Quiz.js" element={<Quiz />} />
+                    <Route path="/Calendar.js" element={<Calendar />} />
+                    <Route path="/Explore.js" element={<PlantList plants={props.plants} />} />
+                    <Route path="/About.js" element={<About />} />
+                </Routes>
+            </div>
         </div>
+    );
 
-        </div>
-    )
 }
