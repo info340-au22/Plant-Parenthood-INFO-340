77e7625aae15897544c14000cc03b6032bf8cb4d
Kriti Vajjhula <avajjh@uw.edu>
2022-11-21 18:54:17 -0800

    explore formatting complete

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -5,18 +5,15 @@ export function PlantCard(props) {
     console.log(props.plants.name);
 
     return(
-        <div>
-            <div className="explore-container">
+        //<div>
+            
                 <div className="explore-item">
                     <h3>{props.plants.name}</h3>
                     <div className="plant-img">
-
-                            <img src={props.plants.img} alt={props.plants.alt}></img>
-
+                        <img src={props.plants.img} alt={props.plants.alt}></img>
                     </div>
                 </div>
-            </div>
-        </div>
+        //</div>
     )
 } 
 
@@ -29,7 +26,9 @@ export function PlantList(props) {
         <div>
             <h1 className="explore-heading">Find your perfect plant!</h1>
             <h2 className="explore-subheading">Search for different species of houseplants in our houseplant index.</h2>
-            {plants}
+            <div className="explore-container">
+                {plants}
+            </div>
         </div>
     )
 }
