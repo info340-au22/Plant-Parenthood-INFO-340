9e49829edeb357a40e4cf1bb79a7bafb1b25cb5e
architasingh <49347272+architasingh@users.noreply.github.com>
2022-12-08 16:19:29 -0800

    Attempt at setting up log-in

---
+++ src/components/SignIn.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Navigate } from 'react-router-dom';
+
+import { getAuth, EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+
+import DEFAULT_USERS from '../data/users.json';
+
+export default function SignIn(props) {
+  const currentUser = props.currentUser;
+  const loginFunction = props.loginCallback;
+
+  //the authenticator
+  const auth = getAuth();
+
+  const configObj = {
+    signInOptions: [
+      { provider: EmailAuthProvider.PROVIDER_ID, requireDisplayName: true },
+      { provider: GoogleAuthProvider.PROVIDER_ID }
+    ],
+    signInFlow: 'popup',
+    callbacks: {
+      signInSuccessWithAuthResult: () => false //don't do anything special on signin
+    },
+    credentialHelper: 'none'
+  }
+
+  // if (currentUser.userId) { //if signed in
+  //   return <Navigate to="/" />
+  // }
+
+  return (
+    <div className="card bg-light">
+      <div className="container card-body">
+        <StyledFirebaseAuth firebaseAuth={auth} uiConfig={configObj} />
+      </div>
+    </div>
+  )
+}
