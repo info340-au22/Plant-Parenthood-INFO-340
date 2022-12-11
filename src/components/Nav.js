198d297bac87661bfb4a06a17182ea3c28348d54
architasingh <49347272+architasingh@users.noreply.github.com>
2022-12-11 14:35:23 -0800

    Add sign out functionality, fix styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,28 +4,30 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import { getAuth, signOut } from 'firebase/auth';
 
 export function PlantNav() {
-  return (
-    <>
-    <Navbar className="color-Nav" variant="dark" expand="lg">
-        <Container>
-            <Navbar.Brand href="/">Plant Parenthood</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav>
-                    <Nav.Link aria-label="Quiz Page" as={Link} to='/QuestionTemplate.js'>Quiz</Nav.Link>
-                    <Nav.Link aria-label="Calendar Page" as={Link} to='/Calendar.js'>Calendar</Nav.Link>
-                    <Nav.Link aria-label="Explore Page" as={Link} to='/Explore.js'>Explore</Nav.Link>
-                    <Nav.Link as={Link} aria-label="About Page" to='/About.js'>About</Nav.Link>
-                    <Button variant="light" aria-label="Sign In" href='/SignIn.js'>Sign In</Button>
-                    {/* <Nav.Item>
-                        <Button variant="light" href='/SignOut.js'>Sign Out</Button>
-                    </Nav.Item> */}
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
-</>
-  );
+    const handleSignOut = (event) => {
+        signOut(getAuth());
+    }
+    return (
+        <>
+            <Navbar className="color-Nav" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">Plant Parenthood</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav>
+                            <Nav.Link aria-label="Quiz Page" as={Link} to='/QuestionTemplate.js'>Quiz</Nav.Link>
+                            <Nav.Link aria-label="Calendar Page" as={Link} to='/Calendar.js'>Calendar</Nav.Link>
+                            <Nav.Link aria-label="Explore Page" as={Link} to='/Explore.js'>Explore</Nav.Link>
+                            <Nav.Link as={Link} aria-label="About Page" to='/About.js'>About</Nav.Link>
+                            <Button variant="light" aria-label="Sign In" href='/SignIn.js'>Sign In</Button>
+                            <Button variant="light" onClick={handleSignOut}>Sign Out</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
