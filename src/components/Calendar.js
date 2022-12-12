9888e80fe49b73406ff6cb42f570252a8bb28ccb
isaxena02 <ishita.saxena1103@gmail.com>
2022-12-11 17:18:26 -0800

    set  default view as day and removed most of the inline styling

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -26,50 +26,57 @@ export function PlantCalendar(props) {
     const [allEvents, setAllEvents] = useState(events);
 
     function handleAddEvent() {
-        
-        for (let i=0; i<allEvents.length; i++){
 
-            const d1 = new Date (allEvents[i].start);
+        for (let i = 0; i < allEvents.length; i++) {
+
+            const d1 = new Date(allEvents[i].start);
             const d2 = new Date(newEvent.start);
             const d3 = new Date(allEvents[i].end);
             const d4 = new Date(newEvent.end);
 
-             if (
-              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-                (d4 <= d3) )
-              )
-            {   
+            if (
+                ((d1 <= d2) && (d2 <= d3)) || ((d1 <= d4) &&
+                    (d4 <= d3))
+            ) {
                 // alert("CLASH"); 
                 // break;
             }
-    
+
         }
-        
-        
+
+
         setAllEvents([...allEvents, newEvent]);
     }
 
     return (
         <div className="App">
-        <h1 className = "calendar-title">Plant Calendar</h1>
-        <h2 className = "calendar-title">Add New Event</h2>
-        <div className = "add-event">
-            <input className = "event-add-title" type="text" placeholder="Add Title" 
-                style={{ width: "20%", marginRight: "10px" }} 
-                value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} 
+            <h1 className="calendar-title">Plant Calendar</h1>
+            <h2 className="calendar-title">Add New Event</h2>
+            <div className="add-event">
+                <input className="event-add-title" type="text" placeholder="Add Title"
+                    value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+                />
+                <DatePicker className="event-date" placeholderText="Start Date"
+                    selected={newEvent.start}
+                    onChange={(start) => setNewEvent({ ...newEvent, start })}
+                />
+                <DatePicker className="event-date" placeholderText="End Date"
+                    selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })}
+                />
+                <button className="event-btn"
+                    onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar
+            className="calendar"
+            localizer={localizer}
+            events={allEvents}
+            startAccessor="start"
+            endAccessor="end"
+            defaultView="day"
+            style={{ height: 500}}
             />
-            <DatePicker placeholderText="Start Date" 
-            style={{ marginRight: "10px" }} selected={newEvent.start} 
-            onChange={(start) => setNewEvent({ ...newEvent, start })} 
-            />
-            <DatePicker placeholderText="End Date" 
-            selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} 
-            />
-            <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
-                Add Event
-            </button>
         </div>
-        <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
-    </div>
     )
 }
