80cf664c2b54a67863cdf45a46d291f127db0f68
Saimanasvi <smcharu@uw.edu>
2022-12-12 18:58:47 -0800

    add captions for images

--- src/components/QuizResultC.js
+++ src/components/QuizResultC.js
@@ -8,12 +8,15 @@ export function QuizResultCPage (props) {
             <div className="plant-info-img">
                 <div className = "quiz-img-column">
                     <img src = "../img/bonsai.png" alt = "Bonsai" Source = "OurHousePlants"/>
+                    <figcaption>Bonsai</figcaption>
                 </div>
                 <div className = "quiz-img-column">
                     <img src = "../img/Calathea.png" alt = "Calathea" Source = "Hirt's Gardens"/>
+                    <figcaption>Calathea</figcaption>
                 </div>
                 <div className = "quiz-img-column">
-                    <img src = "../img/Croton.png" alt = "Croton" Source = "Jackson Perkins" />                    
+                    <img src = "../img/Croton.png" alt = "Croton" Source = "Jackson Perkins" />    
+                    <figcaption>Croton</figcaption>                
                 </div>
             </div>
             <p className = "result-descrip">Looks like we got a plant care expert in the house! You don't mind plants that require the perfect amount of lighting, watering, and temperature to thrive. You're ready to take on a challenge - we love to see it. These are our recommendations for you: Bonsai, Calathea, and Croton!</p>
