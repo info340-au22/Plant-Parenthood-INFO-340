db2376de9e5f982c57f851034630aa3eedb582fd
Saimanasvi <smcharu@uw.edu>
2022-12-12 20:37:33 -0800

    updated quiz result description

--- src/components/QuizResultA.js
+++ src/components/QuizResultA.js
@@ -19,7 +19,7 @@ export function QuizResultAPage (props) {
                     <figcaption>Cacti</figcaption>
                 </div>
             </div>
-            <p className = "result-descrip">You're interested in a plant that will require low effort. Maybe you're a newbie who doesn't have much experience, or maybe you've had your fair share of plant care experience but you like a plant that doesn't require too much of a fuss. These are our recommendations for you: Aglaonema, Baby Rubber Plant and Cacti!</p>
+            <p className = "result-descrip">You're interested in a plant that will require low effort. Maybe you're a newbie who doesn't have much experience, or maybe you've had your fair share of plant care experience but you like a plant that doesn't require too much of a fuss. These are our recommendations for you: Aglaonema, Baby Rubber Plant and Cacti! Refer to our Explore Page for more information!</p>
         </div>
     )
 }
