080bda933cd3b46efc8faa234b4cf9e68c5130b5
Saimanasvi <smcharu@uw.edu>
2022-12-12 16:50:12 -0800

    quiz result styling

--- src/components/QuizResultA.js
+++ src/components/QuizResultA.js
@@ -10,13 +10,15 @@ export function QuizResultA (props) {
     let plant =  _.find(PLANT_DATA, {name: plantNameString.plantName}); //find plant in data
 
     return (
-        <div className="container plant-info-img-container">
+        <div className="explore-container">
             <h1 className = "plant-info-heading">Low Maintenace Plants Would Fit You Best!</h1>
-            <div className="plant-info-img">
-                <img src = "../img/aglaonema.png" alt = "Aglaonema" Source = "plants.com"/>
-                <img src = "../img/babyrubberplant.png" alt = "Baby Rubber Plant" Source = "Green Door Garden"/>
-                <img src = "../img/cacti.png" alt = "Cacti" Source = "House Beautiful" />
-            </div>
+            {/* <div className = "row"> */}
+                <div className="row plant-info-img">
+                    <img src = "../img/aglaonema.png" alt = "Aglaonema" Source = "plants.com"/>
+                    <img src = "../img/babyrubberplant.png" alt = "Baby Rubber Plant" Source = "Green Door Garden"/>
+                    <img src = "../img/cacti.png" alt = "Cacti" Source = "House Beautiful" />
+                </div>
+            {/* </div> */}
             <p>A low maintenance plant would fit you best. You're interested in a plant that will require low effort. Maybe you're a newbie who doesn't have much experience, or maybe you've had your fair share of plant care experience but you like a plant that doesn't require too much of a fuss. These are our recommendations for you: Aglaonema, Baby Rubber Plant and Cacti!</p>
         </div>
     )
